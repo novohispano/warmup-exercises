36b2fc721c766f35a7e37b54ae0d9f2ef2836457
novohispano@gmail.com <novohispano@gmail.com>
2013-02-12 09:20:38 -0700

    Drill response

---
+++ 02-leap-year/leap_year.rb
@@ -0,0 +1,25 @@
+class LeapYear
+  def run
+    puts "Welcome to Year Leap Calculator"
+    command = ""
+    while command != "quit"
+      printf "enter the year: "
+      input = gets.chomp
+      @parts = input.split(" ")
+      command = @parts[0]
+      leap_year(command)
+    end
+  end
+
+  def leap_year(year)
+    case year
+    when year % 4 == 0 then puts "#{year} it's a leap year"
+    when year % 100 == 0 then puts "#{year} it's a leap year"
+    when year % 400 == 0 then puts "#{year} it's a leap year"
+    else puts "#{year} it's not a leap year"
+    end
+  end
+end
+
+leap_year = LeapYear.new
+leap_year.run
