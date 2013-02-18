e065760f70d89deec038a9e0dfc169f252642c1a
novohispano@gmail.com <novohispano@gmail.com>
2013-02-18 08:53:18 -0700

    Some changes

--- 04-bob/bob.rb
+++ 04-bob/bob.rb
@@ -1,10 +1,10 @@
 class Bob
 
   def run
-    puts "Yo! Dude, this is Bob."
+    puts "Yo, dude! This is Bob."
     input = ""
     while input != "quit"
-      printf "talk to Bob: "
+      printf "Talk to Bob: "
       input = gets.chomp
 
       if input == "" then puts "Fine. Be that way!"
