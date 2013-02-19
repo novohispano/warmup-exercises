3d1e5d284976393d682145921247186b58d9f62d
novohispano@gmail.com <novohispano@gmail.com>
2013-02-19 09:37:47 -0700

    Value stored but reset doesn't work

--- 06-robot-name/robot.rb
+++ 06-robot-name/robot.rb
@@ -1,9 +1,23 @@
 class Robot
-  def name
-    name = "#{('A'..'Z').to_a.sample}#{('A'..'Z').to_a.sample}#{rand(0..9)}#{rand(0..9)}"
-    puts name
+  attr_reader :name
+
+  def initialize
+    @name = random_name
+  end
+  
+  def reset
+    puts "Resetting to factory settings."
+    name = random_name
+  end
+
+  def random_name
+    "#{('A'..'Z').to_a.sample}#{('A'..'Z').to_a.sample}#{rand(0..9)}#{rand(0..9)}"
   end
 end
 
 robot1 = Robot.new
-robot1.name
+puts robot1.name
+puts robot1.name
+puts robot1.name
+puts robot1.reset
+puts robot1.name
