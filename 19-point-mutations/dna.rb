d1205de473b44984a06b6c69f61d95d86331cdab
novohispano@gmail.com <novohispano@gmail.com>
2013-03-19 09:27:09 -0600

    all tests passing but one

---
+++ 19-point-mutations/dna.rb
@@ -0,0 +1,22 @@
+class DNA
+  def initialize(strand)
+    @strand = strand.split('')
+  end
+
+  def hamming_distance(sample_strand)
+    split_strand = sample_strand.split('')
+    sum = 0
+    position = 0
+
+    length = @strand.count
+
+    @strand.first(length).each do |char|
+      if char != split_strand[position]
+        sum += 1
+      end
+      position += 1
+      sum
+    end
+    sum
+  end
+end
