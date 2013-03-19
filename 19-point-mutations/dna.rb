5f1832fb981ba23809457eb17cfc1dc4c73e60f0
novohispano@gmail.com <novohispano@gmail.com>
2013-03-19 10:04:43 -0600

    A little refactor

--- 19-point-mutations/dna.rb
+++ 19-point-mutations/dna.rb
@@ -6,16 +6,13 @@ class DNA
   def hamming_distance(sample_strand)
     split_strand = sample_strand.split('')
     sum = 0
-    position = 0
 
     length = split_strand.count
 
-    @strand.first(length).each do |char|
-      if char != split_strand[position]
+    @strand.first(length).each_with_index do |char, index|
+      if char != split_strand[index]
         sum += 1
       end
-      position += 1
-      sum
     end
     sum
   end
