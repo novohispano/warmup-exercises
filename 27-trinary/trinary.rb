f2f9d57967a97c017026593a09e3571b714ffbd0
novohispano@gmail.com <novohispano@gmail.com>
2013-04-05 09:55:27 -0600

    Solved warmup 27

--- 27-trinary/trinary.rb
+++ 27-trinary/trinary.rb
@@ -2,16 +2,14 @@ class Trinary
   attr_reader :numbers
 
   def initialize(input)
-    @numbers = input.split("").collect{ |chr| chr.to_i }
+    @numbers = input.split("").collect{ |chr| chr.to_i }.reverse
   end
 
   def to_decimal
     sum = 0
-    numbers.each_with_index do |number, index|
-        numbers[index] = number
-    end.reduce(:+)
+    numbers.each_with_index do |number, i|
+      sum += number * (3 ** i)
+    end
+    sum
   end
 end
-
-# get the positions in the array
-# get 
