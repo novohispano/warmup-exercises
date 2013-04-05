1ce92121d31e207c80a9399e899ac45f8b950266
novohispano@gmail.com <novohispano@gmail.com>
2013-04-05 09:51:49 -0600

    solved

--- 26-binary/binary.rb
+++ 26-binary/binary.rb
@@ -1,16 +1,15 @@
 class Binary
-  attr_reader :number
+  attr_accessor :numbers
 
   def initialize(input)
-    @number = input.split("")
+    @numbers = input.split("").collect{ |chr| chr.to_i }.reverse
   end
 
   def to_decimal
     sum = 0
-    number.each_with_index do |binary, index| 
-      if binary == "1"
-        sum += 1
-      return sum
+    @numbers.each_with_index do |number, i|
+      sum += number * (2 ** i)
     end
+    sum
   end
 end
