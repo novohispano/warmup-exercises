cf75995549b883fac6571ef734b69874d7cdf8ad
novohispano@gmail.com <novohispano@gmail.com>
2013-04-24 09:35:13 -0600

    Anagram working

---
+++ 31-anagram/anagram.rb
@@ -0,0 +1,25 @@
+class Anagram
+  attr_accessor :word
+
+  def initialize(input)
+    @word = splitter(input)
+  end
+
+  def match(anagrams)
+    result = []
+
+    anagrams.each do |anagram|
+      comparison = splitter(anagram)
+
+      if comparison.eql?(word)
+        result << anagram
+      end
+    end
+
+    return result
+  end
+
+  def splitter(input)
+    input.split('').sort
+  end
+end
